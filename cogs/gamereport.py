50f31ecc26b461835654c1fa6267a7f5a0e14b27
landen <landenj927@gmail.com>
2026-04-01 03:54:51 -0400

    fix git

--- cogs/gamereport.py
+++ cogs/gamereport.py
@@ -127,7 +127,7 @@ class GameReport(commands.Cog):
                         qb.get("comp", 0),
                         qb.get("yds", 0),
                         qb.get("td", 0),
-                        qb.get("int", 0)
+                        qb.get("int", qb.get("ints", 0))
                     )
 
                 # WR
