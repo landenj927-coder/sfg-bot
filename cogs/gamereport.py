53536f953f9806bd1a37d7db2bd8069f7671adc6
landen <landenj927@gmail.com>
2026-04-01 04:02:01 -0400

    fix

--- cogs/gamereport.py
+++ cogs/gamereport.py
@@ -124,10 +124,11 @@ class GameReport(commands.Cog):
                     append_qb_statline(
                         name,
                         team_name,
+                        qb.get("qbr", qb.get("rtng", 0)),
                         qb.get("comp", 0),
                         qb.get("yds", 0),
                         qb.get("td", 0),
-                        qb.get("ints") if qb.get("ints") is not None else qb.get("int", 0)
+                        qb.get("int", qb.get("ints", 0))
                     )
 
                 # WR
